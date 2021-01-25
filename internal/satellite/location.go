c7fb8910145ef7f66f50f805dcce286fc0a2e96a
Andre Billia <andrebillia@gmail.com>
2021-01-25 09:32:59 -0300

    fix intersection circles calculation

--- internal/satellite/location.go
+++ internal/satellite/location.go
@@ -2,6 +2,7 @@
 package satellite
 
 import (
+	"errors"
 	"math"
 )
 
@@ -15,91 +16,112 @@ type circle struct {
 	R float64
 }
 
-func intersect(a *circle, b *circle) (p []point) {
-	dx, dy := b.X-a.X, b.Y-a.Y
-	lr := a.R + b.R                //radius and
-	dr := math.Abs(a.R - b.R)      //radius difference
-	ab := math.Sqrt(dx*dx + dy*dy) //center distance
-
-	if ab <= lr && ab > dr {
-		theta1 := math.Atan(dy / dx)
-		ef := lr - ab
-		ao := a.R - ef/2
-		theta2 := math.Acos(ao / a.R)
-		theta := theta1 + theta2
-		xc := a.X + a.R*math.Cos(theta)
-		yc := a.Y + a.R*math.Sin(theta)
-		p = append(p, point{xc, yc})
-
-		if ab < lr { //two intersections
-			theta3 := math.Acos(ao / a.R)
-			theta = theta3 - theta1
-			xd := a.X + a.R*math.Cos(theta)
-			yd := a.Y - a.R*math.Sin(theta)
-			p = append(p, point{xd, yd})
-		}
-	}
-	return p
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
 }
 
-func matchesBetweenArrays(satellites [3][]point) (p []point) {
-	intersec := make(map[point]bool)
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
 
-	for index := 0; index < len(satellites); index++ {
-		if len(satellites[index]) == 0 {
-			return
+func calculateThreeCircleIntersection(c [3]circle) (point, error) {
+
+	p := point{}
+
+	/* dx and dy are the vertical and horizontal distances between
+	* the circle centers.
+	 */
+	dx := c[1].point.X - c[0].point.X
+	dy := c[1].point.Y - c[0].point.Y
+
+	/* Determine the straight-line distance between the centers. */
+	d := math.Sqrt((dy * dy) + (dx * dx))
+
+	/* Check for solvability. */
+	if d > (c[0].R + c[1].R) {
+		e := errors.New("circles do not intersect")
+		return p, e
+	}
+	if d < math.Abs(c[0].R-c[1].R) {
+		e := errors.New("one circle is contained in the other")
+		return p, e
+	}
+
+	/* 'point 2' is the point where the line through the circle
+	* intersection points crosses the line between the circle
+	* centers.
+	 */
+
+	/* Determine the distance from point 0 to point 2. */
+	a := ((c[0].R * c[0].R) - (c[1].R * c[1].R) + (d * d)) / (2.0 * d)
+
+	/* Determine the coordinates of point 2. */
+	point2x := c[0].point.X + (dx * a / d)
+	point2y := c[0].point.Y + (dy * a / d)
+
+	/* Determine the distance from point 2 to either of the
+	* intersection points.
+	 */
+	h := math.Sqrt((c[0].R * c[0].R) - (a * a))
+
+	/* Now determine the offsets of the intersection points from
+	* point 2.
+	 */
+	rx := -dy * (h / d)
+	ry := dx * (h / d)
+
+	/* Determine the absolute intersection points. */
+	intersectionPoint1x := toFixed(point2x+rx, 3)
+	intersectionPoint2x := toFixed(point2x-rx, 3)
+	intersectionPoint1y := toFixed(point2y+ry, 3)
+	intersectionPoint2y := toFixed(point2y-ry, 3)
+
+	// fmt.Println("INTERSECTION Circle1 AND Circle2: (", intersectionPoint1x, ",", intersectionPoint1y, ")", " AND (", intersectionPoint2x, ",", intersectionPoint2y, ")")
+
+	/* Lets determine if circle 3 intersects at either of the above intersection points. */
+	dx = intersectionPoint1x - c[2].point.X
+	dy = intersectionPoint1y - c[2].point.Y
+	d1 := math.Sqrt((dy * dy) + (dx * dx))
+
+	dx = intersectionPoint2x - c[2].point.X
+	dy = intersectionPoint2y - c[2].point.Y
+	d2 := math.Sqrt((dy * dy) + (dx * dx))
+
+	if math.Abs(d1-c[2].R) < 0.001 {
+		p = point{
+			X: intersectionPoint1x,
+			Y: intersectionPoint1y,
 		}
-	}
-
-	for _, item := range satellites[0] {
-		intersec[item] = true
-	}
-
-	for _, item := range satellites[1] {
-		if _, ok := intersec[item]; ok {
-			p := append(p, item)
-			_ = p
+	} else if math.Abs(d2-c[2].R) < 0.001 {
+		p = point{
+			X: intersectionPoint2x,
+			Y: intersectionPoint2y,
 		}
+	} else {
+		e := errors.New("one of 3 circles didn't intersects")
+		return p, e
 	}
 
-	for _, item := range satellites[2] {
-		if _, ok := intersec[item]; ok {
-			p = append(p, item)
-		}
-	}
-
-	return
+	return p, nil
 }
 
 // GetLocation receives data from all satellites and tries to find ship position
 func GetLocation(satellites []Data) (x float32, y float32, found bool) {
 
 	// Populate circles with point (Location) and Radius (Distances)
-	circle := [3]circle{}
+	circles := [3]circle{}
 	for index := 0; index < len(satellites); index++ {
 		var key string = satellites[index].Name
-		circle[index].point.X = float64(satellitesLocation[key].X)
-		circle[index].point.Y = float64(satellitesLocation[key].Y)
-		circle[index].R = float64(satellites[index].Distance)
+		circles[index].point.X = float64(satellitesLocation[key].X)
+		circles[index].point.Y = float64(satellitesLocation[key].Y)
+		circles[index].R = float64(satellites[index].Distance)
 	}
 
-	// Find every intersection between two circles (satellites)
-	intersecBetweenTwocircles := [3][]point{}
-	for index := 0; index < len(intersecBetweenTwocircles); index++ {
-		intersecBetweenTwocircles[index] = intersect(&circle[index%len(intersecBetweenTwocircles)],
-			&circle[(index+1)%len(intersecBetweenTwocircles)])
+	p, err := calculateThreeCircleIntersection(circles)
+	if err != nil {
+		return 0, 0, false
 	}
 
-	// Find common value between all inntersections otherwise return
-	var p []point = matchesBetweenArrays(intersecBetweenTwocircles)
-	if len(p) == 0 {
-		x = 0
-		y = 0
-	} else {
-		found = true
-		x = float32(p[0].X)
-		y = float32(p[0].Y)
-	}
-
-	return
+	return float32(p.X), float32(p.Y), true
 }
